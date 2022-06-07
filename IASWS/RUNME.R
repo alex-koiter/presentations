36a803b442d2c68119eb56a36d692b5cb16e68dd
Alex Koiter <akoiter@gmail.com>
2022-06-07 12:17:48 -0500

    reduced pdf size

--- IASWS/RUNME.R
+++ IASWS/RUNME.R
@@ -1,15 +1,10 @@
 
-f <- "IAWSW_2022"
 
-# Open in Chrome
-#system(paste0("google-chrome '", f, ".html'"))
+pagedown::chrome_print("IASWS_2022.html", "IASWS_Koiter_2022.pdf", timeout = 120) 
 
-# Save PDF through Chrome Headless (puppeteer)
-l <- normalizePath(list.files(pattern = paste0(f, ".html"), full.names = TRUE))
-
-system(paste0("node ./Print_PDF/print_pdf.js 'file://", l, "' 3000 '", paste0(f, ".pdf"), "'"))
-
+# NOT necessary here
 # Reduce size (prepress = 300 dpi, printer = 300 dpi, ebook = 150 dpi, screen = 72dpi)
-system(paste0("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile='", f, "_small.pdf' '", f, ".pdf'"))
-
-
+system(paste0("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 ",
+              "-dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ",
+              "-sOutputFile='IASWS_Koiter_2022_sm.pdf' ",
+              "'IASWS_Koiter_2022.pdf'"))
