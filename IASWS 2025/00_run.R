42da86d0c3283e1c4053a1fb9692823a1cfa0269
alex-koiter <akoiter@gmail.com>
2025-06-13 10:22:33 -0500

    PDF and clean up

--- IASWS 2025/00_run.R
+++ IASWS 2025/00_run.R
@@ -1,12 +1,13 @@
-quarto::quarto_render("intro_to_rmarkdown.qmd")
 
-pagedown::chrome_print("intro_to_rmarkdown.html",
-                       output = "intro_to_rmarkdown.pdf",
+
+pagedown::chrome_print("./IASWS 2025/IASWS_2025.html",
+                       output = "./IASWS 2025/IASWS_2025.pdf",
+                       options = list(printBackground = TRUE),
                        extra_args = "--font-render-hinting=none")
 
 
 
-system(glue::glue("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 ",
-                  "-dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ",
-                  "-sOutputFile='intro_to_rmarkdown_sm.pdf' ",
-                  "'intro_to_rmarkdown.pdf'"))
+# system(glue::glue("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 ",
+#                   "-dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ",
+#                   "-sOutputFile='intro_to_rmarkdown_sm.pdf' ",
+#                   "'intro_to_rmarkdown.pdf'"))
